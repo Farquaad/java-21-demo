978d03548752281ba5d51b502d6638ead2ac1f1e
Balázs Wittner <wittner.balazs@microsec.hu>
2023-03-16 09:45:12 +0100

    Initial commit

---
+++ src/main/java/hu/microsec/java21demo/entity/Post.java
@@ -0,0 +1,32 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "blog_post")
+public class BlogPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String author;
+
+    @Column(name = "creation_date_time")
+    private LocalDateTime creationDateTime;
+
+    @Column(name = "update_date_time")
+    private LocalDateTime updateDateTime;
+
+    
+
---
+++ src/test/java/hu/microsec/java21demo/Java21DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.microsec.java21demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
